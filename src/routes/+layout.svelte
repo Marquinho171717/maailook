b78502438e108719bef68e133716b41906faa225
AndreyHP <andreymarins501@gmail.com>
2025-01-19 10:02:26 -0300

    atualizei a label dos links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,11 +7,11 @@
 <header>
 	<div id="menu-bar">
     <nav>
-	    <a href="{base}/" class="menu-link" title="home">
-                <img src="./icons/logo.png" id="img-logo" alt="sakura">
+	    <a href="{base}/" class="menu-link" title="home" aria-label="Voltar para pagina Inicial">
+                <img src="./icons/logo.png" id="img-logo" alt="Maailook">
         </a>
-	    <a href="{base}/about" class="menu-link" title="settings">
-                <img src="./icons/cart-shopping-solid.png" id="settings-logo" alt="settings">
+	    <a href="{base}/about" class="menu-link" title="cart" aria-label="Abrir carrinho">
+                <img src="./icons/cart-shopping-solid.png" id="settings-logo" alt="carrinho">
         </a>
     </nav>
     </div>
